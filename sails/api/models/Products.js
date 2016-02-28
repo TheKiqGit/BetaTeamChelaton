065f9b6fefd4f1778c9580d0f91440a93550ea49
Enrique Ortega Cardoso <enrique.ortegacard@outlook.com>
2016-02-27 19:32:13 -0600

    Se termina el modelo de productos

--- sails/api/models/Products.js
+++ sails/api/models/Products.js
@@ -1,13 +1,44 @@
 /**
  * Products.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: This model stores the product's data.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
   attributes: {
 
+  	tableName: 'products',
+  	id: {
+  		type: 'integer',
+  		unique: true,
+  		primaryKey: true
+  	},
+  	name: {
+  		type: 'string',
+  		defaultsTo: 'cervezaTmp'
+  	},
+  	price: {
+  		type: 'float',
+  		defaultsTo: 0
+  	},
+  	type: {
+  		type: 'string',
+  		defaultsTo: 'ambar',
+  		enum : ['ambar', 'oscura', 'clara', 'lager']
+  	},
+  	createdAt: {
+  		type: 'date',
+  		defaultsTo: function (){
+  			return new Date().toLocaleString();
+  		}
+  	},
+  	updatedAt: {
+  		type: 'date',
+  		defaultsTo: function (){
+  			return new Date().toLocaleString();
+  		}
+  	}
   }
 };
 
